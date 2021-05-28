74bb39a1b8ccc90d5032c856c6931fc75719a371
FrancoCaneti <CAnetifranco95@hmail.com>
2021-05-28 16:27:51 +0200

    stanze

--- partials/script/get-stanze.php
+++ partials/script/get-stanze.php
@@ -1,12 +1,12 @@
-<?
-require_once __DIR__ .'/database.php';
+<?php
+require_once __DIR__ . '/database.php';
 
-$sql = "SELECT `id`,`room_number` , `floor`', `beds` FROM `stanze`";
+$sql = "SELECT `id`, `room_number`, `floor`,`beds` FROM `stanze`";
 $result = $conn->query($sql);
 
 if($result && $result->num_rows > 0) {
-    $stanze= [];
-    while($row = $result->fetch_assoc()){
+    $stanze = [];
+    while($row = $result->fetch_assoc()) {
         $stanze[] = $row;
     }
     var_dump($stanze);
