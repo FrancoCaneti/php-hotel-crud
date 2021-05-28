3c68a550e1a177c3741257af6f8bb99808ea53d5
FrancoCaneti <CAnetifranco95@hmail.com>
2021-05-28 15:27:59 +0200

    connessione db

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> PHP e MYSQL - Hotel stanze</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php 
+//connessione database
+
+$host = 'localhost';
+$username ='root';
+$password = 'root';
+$db_name = 'hotel';
+
+//connection
+
+$con = new mysqli($host, $username, $password, $db_name);
+
+//check
+
+if ($conn && $conn->connected_error) {
+    die("Connected failed: $conn->connection_error");
+}
+
+
+?>
